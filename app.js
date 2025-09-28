3e7c696fc3c5fe95241f214e081bf6fddc8f2fac
Gustavo <gustavo-dm2010@hotmail.com>
2025-09-28 00:18:40 -0300

    Adicionado operações aritméticas e iniciado resposividade

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const saida = document.getElementById("saida");
+const calc = document.getElementById("calc");
 let teclas = document.getElementsByClassName("teclas");
 
 const eventos = ["click", "keydown"];
@@ -8,54 +9,168 @@ for (let n = 0; n < teclas.length; n++) {
     eventos.forEach(evento => {
 
         teclas[n].addEventListener(evento, event => {
-            console.log(event);
+
             const tecla = event.target.id;
     
             switch(tecla){
+                case "botao-divide":
+                    novaEntrada = true;
+                    let divisao;
+                    if (calc.innerText != '') {
+                       divisao = Divisao(calc, saida);
+                       calc.innerHTML = divisao + ' / ';
+                       saida.innerText = divisao;
+                       break;
+                    }
+                    calc.innerHTML = saida.innerText + ' / ';
+                    break;
+                    
+                case "botao-times":
+                    novaEntrada = true;
+                    let multiplicacao;
+                    if (calc.innerText != '') {
+                       multiplicacao = Multiplicacao(calc, saida);
+                       calc.innerHTML = multiplicacao + ' x ';
+                       saida.innerText = multiplicacao;
+                       break;
+                    }
+                    calc.innerHTML = saida.innerText + ' x ';
+                    break;
+
+                case "botao-minus":
+                    novaEntrada = true;
+                    let subtracao;
+                    if (calc.innerText != '') {
+                       subtracao = Subtracao(calc, saida);
+                       calc.innerHTML = subtracao + ' - ';
+                       saida.innerText = subtracao;
+                       break;
+                    }
+                    calc.innerHTML = saida.innerText + ' - ';
+                    break;
+
+                case "botao-plus":
+                    novaEntrada = true;
+                    let soma;
+                    if (calc.innerText != '') {
+                       soma = Soma(calc, saida);
+                       calc.innerHTML = soma + ' + ';
+                       saida.innerText = soma;
+                       break;
+                    }
+                    calc.innerHTML = saida.innerText + ' + ';
+                    break;
+
+                case "botao-equal":
+                    novaEntrada = true;
+                    
+                    if(calc.innerText.slice(-1) == '+') {
+                        const resultado = Soma(calc, saida);
+                        calc.innerHTML += saida.innerText + ' = ';
+                        saida.innerText = resultado;
+                    } 
+                    else if (calc.innerText.slice(-1) == '-') {
+                        const resultado = Subtracao(calc, saida);
+                        calc.innerHTML += saida.innerText + ' = ';
+                        saida.innerText = resultado;
+                    }
+                    else if (calc.innerText.slice(-1) == 'x') {
+                        const resultado = Multiplicacao(calc, saida);
+                        calc.innerHTML += saida.innerText + ' = ';
+                        saida.innerText = resultado;
+                    }
+                    else if (calc.innerText.slice(-1) == '/') {
+                        const resultado = Divisao(calc, saida);
+                        calc.innerHTML += saida.innerText + ' = ';
+                        saida.innerText = resultado;
+                    }
+                    break;
+                    
+                case "botao-invert":
+                    calc.innerText = '';
+                    saida.innerText = -saida.innerText;
+                    break;
+                case "botao-clear":
+                    calc.innerText = '';
+                    saida.innerText = 0;
+                    break;
                 case "botao-clear-entry":
                     saida.innerText = 0;
                     break;
+                case "botao-backspace":
+                    if(saida.innerText.length == 1){
+                        saida.innerText = 0;
+                    } else {
+                        saida.innerText = saida.innerText.slice(0, -1);
+                    }
+                    break;
                 case "botao-1":
-                    alteraValor(1);
+                    AlteraValor(1);
                     break;
                 case "botao-2":
-                    alteraValor(2);
+                    AlteraValor(2);
                     break;
                 case "botao-3":
-                    alteraValor(3);
+                    AlteraValor(3);
                     break;
                 case "botao-4":
-                    alteraValor(4);
+                    AlteraValor(4);
                     break;
                 case "botao-5":
-                    alteraValor(5);
+                    AlteraValor(5);
                     break;
                 case "botao-6":
-                    alteraValor(6);
+                    AlteraValor(6);
                     break;
                 case "botao-7":
-                    alteraValor(7);
+                    AlteraValor(7);
                     break;
                 case "botao-8":
-                    alteraValor(8);
+                    AlteraValor(8);
                     break;
                 case "botao-9":
-                    alteraValor(9);
+                    AlteraValor(9);
                     break;
                 case "botao-0":
-                    alteraValor(0);
+                    AlteraValor(0);
                     break;
             }
-        })
+        });
     })
 }
 
-const alteraValor = (saida, valor) => {
-    saida.innerText == 0 ? saida.innerText = valor : saida.innerText += valor;
+var novaEntrada = true;
+
+const AlteraValor = (valor) => {
+
+    if(calc.innerText.indexOf("=") != -1){
+        calc.innerText = saida.innerText;
+
+    }
+
+    if(saida.innerText == 0 || novaEntrada && calc.innerText != ''){
+        saida.innerText = valor;
+        novaEntrada = false;
+    } else {
+        saida.innerText += valor;
+    } 
 }
 
-const teclaPressionada = (event, tecla, saida, valor) => {
-    if (event.key == tecla || event.target){
-        alteraValor(saida, valor);
-    }
+const Soma = (calc, saida) => {
+    return Number(calc.innerText.slice(0, -2)) + Number(saida.innerText);
+}
+
+const Subtracao = (calc, saida) => {
+
+    return Number(calc.innerText.slice(0, -2)) - Number(saida.innerText);
+}
+
+const Multiplicacao = (calc, saida) => {
+
+    return Number(calc.innerText.slice(0, -2)) * Number(saida.innerText);
+}
+
+const Divisao = (calc, saida) => {
+
+    return Number(calc.innerText.slice(0, -2)) / Number(saida.innerText);
 }
